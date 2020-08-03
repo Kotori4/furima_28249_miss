f1e9638f8f1fbe6a00e9edf2c3f8209ca083d0a7
Kotori4 <68325394+Kotori4@users.noreply.github.com>
2020-08-03 21:23:50 +0900

    テスト単体コード実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
+    password              {password}
     password_confirmation {password}
     last_name             {Faker::lastName}
     first_name            {Faker::firstName}
