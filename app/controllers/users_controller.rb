4e1f4555abe6e04a5088e107438b822e2678b695
Kotori4 <68325394+Kotori4@users.noreply.github.com>
2020-08-03 13:05:35 +0900

    未・ログイン時の表示変化の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  def create
+  def new
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:create, :edit, :update]
+  resources :users, only: [:new, :edit, :update]
 end
