8c8a331514f988af6207558052c4222a87382515
Kotori4 <68325394+Kotori4@users.noreply.github.com>
2020-08-03 11:26:02 +0900

    ビューファイルの配置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_paramerters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_paramerters
+    devise_parameter_sanitizer.permit(:sign_up, key: [:name])
+  end
 end
